7c12873e3ff7e4b1ffa6b10ae4baa20984350053
Naru8521 <215614425+Mars-Server@users.noreply.github.com>
2025-06-29 12:04:09 +0900

    Update serverDownloadURL.ts

--- src/config/serverDownloadURL.ts
+++ src/config/serverDownloadURL.ts
@@ -4,11 +4,13 @@ export enum OsType {
 }
 
 export enum BasicVersions {
-    "1.21.84.1" = "1.21.84.1"
+    "1.21.84.1" = "1.21.84.1",
+    "1.21.92.1" = "1.21.92.1"
 }
 
 export enum PreviewVersions {
-    "1.21.90.28" = "1.21.90.28"
+    "1.21.90.28" = "1.21.90.28",
+    "1.21.100.22" = "1.21.100.22"
 }
 
 export const serverDownloadURL: Record<string, string> = {
